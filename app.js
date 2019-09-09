f6d49355169debceb54ad6944b1d71459c54c5a0
Larissa <larissa@Xense8.local>
2019-09-09 10:24:54 -0500

    Add routhes and views of AUTH(signup & login)

--- app.js
+++ app.js
@@ -67,7 +67,10 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 const index = require('./routes/index');
-app.use('/', index);
+app.use('/', index)
+
+const auth = require('./routes/auth')
+app.use('/', auth)
 
 
 module.exports = app;
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,21 @@
 const router = require('express').Router()
 
-router.ger('/signup', (req,res,next) => {
-	res.render('auth/form')
+router.get('/signup', (req,res,next) => {
+	const config = {
+		title: 'Sign up',
+		action: '/signup',
+		button: 'Sign up'
+	}
+	res.render('auth/form', config) //info dinámica en auth/form
 })
 
-router.get('7login', (req,res, next) => {
-	res.render('auth/form')
+router.get('/login', (req,res, next) => {
+	const config = {
+		title: 'Log in',
+		action: '/login',
+		button: 'Log in'
+	}
+	res.render('auth/form', config)
 })
 
 module.exports = router
